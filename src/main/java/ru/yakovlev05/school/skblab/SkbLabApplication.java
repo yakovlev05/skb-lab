226fe835d48a9ff487ceb59acec31d953af292c9
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-02-18 21:10:06 +0500

    Create project

---
+++ src/main/java/ru/yakovlev05/school/skblab/SkbLabApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.school.skblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkbLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yakovlev05/school/skblab/SkbLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.school.skblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
